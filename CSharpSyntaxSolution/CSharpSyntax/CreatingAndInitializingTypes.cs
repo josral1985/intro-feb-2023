700b1a3093389738f2930833549a129e092c275e
Jose R Hernandez <jose_r_hernandez@progressive.com>
2023-02-07 16:38:37 -0500

    practice

--- CSharpSyntaxSolution/CSharpSyntax/CreatingAndInitializingTypes.cs
+++ CSharpSyntaxSolution/CSharpSyntax/CreatingAndInitializingTypes.cs
@@ -1,4 +1,6 @@
-﻿namespace CSharpSyntax;
+﻿using System.Text;
+
+namespace CSharpSyntax;
 
 public class CreatingAndInitializingTypes
 {
@@ -53,9 +55,64 @@ public class CreatingAndInitializingTypes
 
         Assert.Equal(message, "old enough");
     }
+
+    [Fact]
+    public void MutableStringsWithStringBuilder()
+    {
+        var message = new StringBuilder();
+
+        foreach(var num in Enumerable.Range(1, 10000))
+        {
+            message.Append(num.ToString() + ", ");
+        }
+
+        Assert.True(message.ToString().StartsWith("1, 2, 3, 4"));
+    }
+
+    [Fact]
+    public void MoreAboutStrings()
+    {
+        var name = "Bob";
+        var age = 33;
+
+        var message = "the name is " + name + " and the age is " + age + ".";
+        var message2 = string.Format("the name is {0} and the age is {1} (again, name: {0}", name, age);
+        var pay = 120_000.00M;
+        var m3 = $"{name} makes {pay:c} a year";
+    }
+
+    [Fact]
+    public void DoingConversionOnTypes()
+    {
+        string myPay = "10000.83Tacos";
+
+        //try
+        //{
+        //    decimal payAsNumber = decimal.Parse(myPay);
+        //    Assert.Equal(10_000.83M, payAsNumber);
+        //}
+        //catch (FormatException)
+        //{
+        //    //that didn't work
+        //}
+
+        if(decimal.TryParse(myPay, out decimal payAsNumber))
+        {
+            Assert.Equal(10_000.83M, payAsNumber);
+        }
+        else
+        {
+            Assert.True(false);
+        }
+
+
+
+    }
 }
 
 public class Taco
 {
 
 }
+
+public class TransitoryPolicyCommuterRecord { }
---
+++ practice/DjBreakTimer/DjBreakTimer/Program.cs
@@ -0,0 +1,47 @@
+﻿TimeSpan breakMinutesAsTimeSpan, songLengthAsTimeSpan;
+
+//getting current time
+var currentTime = DateTimeOffset.Now;
+
+
+//Iter 1
+while (true)
+{
+    Console.WriteLine("How long is the break: ");
+    var breakTime = "00:" + Console.ReadLine() + ":00";
+    if (TimeSpan.TryParse(breakTime, out breakMinutesAsTimeSpan))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Please enter how minutes; a number.");
+    }
+}
+
+while (true)
+{
+    Console.WriteLine("How long is the song: ");
+    var songLength = "00:" + Console.ReadLine();
+    if (TimeSpan.TryParse(songLength, out songLengthAsTimeSpan))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Please enter MIN:SEC");
+    }
+}
+
+//getting current Time
+//Console.WriteLine("CurrentTime: " + currentTime);
+
+Console.WriteLine("Start of break is " + currentTime.Hour + ":" + currentTime.Minute + " and your break is " + breakMinutesAsTimeSpan.Minutes + " minutes long");
+Console.WriteLine("The song is " + songLengthAsTimeSpan.Minutes + " minutes and " + songLengthAsTimeSpan.Seconds + " seconds long");
+
+var endOfBreak = currentTime.AddMinutes(breakMinutesAsTimeSpan.TotalMinutes);
+Console.WriteLine("End of Break: " + endOfBreak.Hour + ":" + endOfBreak.Minute + ":" + endOfBreak.Second);
+
+var startSong = (endOfBreak - songLengthAsTimeSpan) - currentTime;
+
+Console.WriteLine("start the song in: " + startSong.Minutes + " minutes and " + startSong.Seconds + " second(s)");
