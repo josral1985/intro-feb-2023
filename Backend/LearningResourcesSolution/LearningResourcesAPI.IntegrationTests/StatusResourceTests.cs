6ec68baccb71398a6de057f661823e76284eab08
Jose R Hernandez <jose_r_hernandez@progressive.com>
2023-02-10 16:23:04 -0500

    LearningResourcesAPI

---
+++ Backend/LearningResourcesSolution/LearningResourcesAPI.IntegrationTests/StatusResourceTests.cs
@@ -0,0 +1,25 @@
+﻿using LearningResourcesAPI.Controllers;
+
+namespace LearningResourcesAPI.IntegrationTests
+{
+    public class StatusResourceTests
+    {
+        [Fact]
+        public async Task TheStatusResource()
+        {
+            await using var host = await AlbaHost.For<Program>();
+
+            var response = await host.Scenario(api => //integration test - usually has many steps
+            {
+                api.Get.Url("/status");
+                api.StatusCodeShouldBeOk(); //200 status code
+            });
+
+            var responseMessage = response.ReadAsJson<GetStatusResponse>();
+
+            Assert.NotNull(responseMessage);
+            Assert.Equal("All's Good", responseMessage.Message);
+            Assert.Equal("555-555-5555", responseMessage.Contact);
+        }
+    }
+}
---
+++ Backend/LearningResourcesSolution/LearningResourcesAPI/Controllers/StatusController.cs
@@ -0,0 +1,36 @@
+﻿using LearningResourcesAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningResourcesAPI.Controllers;
+public class StatusController : ControllerBase
+{
+    private ISystemTime _systemTime;
+
+    public StatusController(ISystemTime systemTime)
+    {
+        _systemTime = systemTime;
+    }
+
+    //GET /status
+    [HttpGet("/status")]
+    public ActionResult GetTheStatus()
+    {
+        var contact = _systemTime.GetCurrent().Hour < 16 ? "bob@aol.com" : "555-555-5555";
+        var res = new GetStatusResponse
+        {
+            Message = "All's Good",
+            Contact = contact
+        };
+
+        return Ok(res);
+    }
+}
+
+public class GetStatusResponse
+{
+    public string Message { get; set; } = string.Empty;
+    public string Contact { get; set; } = string.Empty;
+}
+
+
+
