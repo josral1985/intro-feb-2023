4ca163b8a923eeaa81a15ad3571a8b5c17ef2f67
Jose R Hernandez <jose_r_hernandez@progressive.com>
2023-02-13 15:20:15 -0500

    We started doing some Angular

---
+++ frontend/src/app/components/shopping-list/shopping-list.component.ts
@@ -0,0 +1,57 @@
+import { compileNgModule } from '@angular/compiler';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css'],
+})
+export class ShoppingListComponent {
+  form = new FormGroup({
+    item: new FormControl<string>('', { nonNullable: true }),
+  });
+
+  items: ShoppingItem[] = [
+    {
+      description: 'Beer',
+      purchased: false,
+    },
+    {
+      description: 'Buns',
+      purchased: false,
+    },
+    {
+      description: 'Eggs',
+      purchased: true,
+    },
+  ];
+
+  clearPurchased() {
+    this.items = this.items.filter((item) => item.purchased === false);
+  }
+
+  get hasPurchasedItems() {
+    return this.items.filter((item) => item.purchased).length > 0;
+  }
+
+  markPurchased(item: ShoppingItem) {
+    item.purchased = true;
+  }
+
+  addItem() {
+    // console.log(this.form.value);
+    const description = this.form.controls.item.value;
+    const newItem: ShoppingItem = {
+      description: description,
+      purchased: false,
+    };
+
+    this.items = [newItem, ...this.items];
+  }
+}
+
+type ShoppingItem = {
+  description: string;
+  purchased: boolean;
+};
