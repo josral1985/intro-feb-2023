6fce5368af13745435f4a02f8e46295bb3581f10
Jose Hernandez <josrhernandez1985@gmail.com>
2023-02-14 19:29:54 -0500

    modify custom exception

--- frontend/src/app/typescriptdemo/kata/NoNegativesAllowedException.ts
+++ frontend/src/app/typescriptdemo/kata/NoNegativesAllowedException.ts
@@ -1,8 +1,1 @@
-export class NoNegativesAllowedException extends Error {
-  __proto__ = Error;
-
-  constructor(msg: string) {
-    super(msg);
-    Object.setPrototypeOf(this, NoNegativesAllowedException.prototype);
-  }
-}
+export class NoNegativesAllowedException extends Error {}
