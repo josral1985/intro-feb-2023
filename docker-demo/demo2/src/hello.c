908e29387e76198c693ae5a556ec8c9840900f7b
Jose R Hernandez <jose_r_hernandez@progressive.com>
2023-02-17 12:34:53 -0500

    Intro to Dockerfile!

---
+++ docker-demo/demo2/src/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello from a container and C");
+
+    return 0;
+}
---
+++ docker-demo/demo3/src/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello from a container and C on Alpine!!!");
+
+    return 0;
+}
