0dc389567225d6159b35a31c42a03686eaf7a0d3
Jose R Hernandez <jose_r_hernandez@progressive.com>
2023-02-17 11:49:05 -0500

    API -> able to get Post Requests and save to DB

--- Backend/LearningResourcesSolution/LearningResourcesAPI/Controllers/ResourcesController.cs
+++ Backend/LearningResourcesSolution/LearningResourcesAPI/Controllers/ResourcesController.cs
@@ -1,4 +1,5 @@
 ﻿using LearningResourcesAPI.Adapters;
+using LearningResourcesAPI.Domain;
 using LearningResourcesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,32 +21,67 @@ namespace LearningResourcesAPI.Controllers
                 .Select(item => new GetResourceItem
                 {
                     Id = item.Id.ToString(),
-                    Description= item.Description,
-                    Link= item.Link,
-                    Type= item.Type,
+                    Description = item.Description,
+                    Link = item.Link,
+                    Type = item.Type,
                 }).ToListAsync();
             var response = new GetResourcesResponse { Items = items };
 
             return Ok(response);
         }
 
+        [HttpGet("/resources/{id:int}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var response = await _context.Items
+                .Where(item => item.Id == id)
+                .Select(item => new GetResourceItem
+                {
+                    Id = item.Id.ToString(),
+                    Description = item.Description,
+                    Link = item.Link,
+                    Type = item.Type,
+                }).SingleOrDefaultAsync();
+
+            //if the response is null, return NotFound()/ 404
+            if (response == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(response);
+            }
+        }
+
 
         [HttpPost("/resources")]
         public async Task<ActionResult> AddItem([FromBody] CreateResourceItem request)
         {
-            if(ModelState.IsValid == false) 
+            if (ModelState.IsValid == false)
             {
                 return BadRequest(ModelState);
             }
-            //tomorrow add it to the database
-            var response = new GetResourceItem
+            //add it to the database
+            //need to make a learningItem first
+            var itemToSave = new LearningItem
             {
-                Id = Guid.NewGuid().ToString(),
                 Description = request.Description,
                 Link = request.Link,
                 Type = request.Type,
             };
-            return Ok(response);
+
+            _context.Items.Add(itemToSave); //add to the context
+            await _context.SaveChangesAsync(); //save to the DB async. it will wait until this is done before continuing
+
+            var response = new GetResourceItem
+            {
+                Id = itemToSave.Id.ToString(), //the ID is auto created by .Add() and .SaveChangesAsync()
+                Description = itemToSave.Description,
+                Link = itemToSave.Link,
+                Type = itemToSave.Type
+            };
+            return StatusCode(201, response);
         }
     }
 }
